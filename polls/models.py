e3069301612c8b38fddfa57394a38363673bf73f
Abdul Aman <kshmr.lg@gmail.com>
2025-02-09 17:07:11 +0400

    Adding more changes to admin page related to the models

--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 import datetime
+from django.contrib import admin
 # Create your models here.
 
 class Question(models.Model):
@@ -10,6 +11,12 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
     
+
+    @admin.display(
+            boolean=True,
+            ordering="pub_date",
+            description="published recently",
+    )
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
